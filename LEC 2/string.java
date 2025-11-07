69dc75750c1abf736fc821ef4c7ee8c139c4f24e
rajpatle2006 <rajpatle799@gmail.com>
2025-11-08 00:09:19 +0530

    Add files via upload
    codes java

---
+++ LEC 2/string.java
@@ -0,0 +1,11 @@
+// package lec 2.java;
+import java.util.Scanner;
+public class string {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter you string value");
+           String str= sc.nextLine();
+           System.out.println(str);
+    }
+           
+}
