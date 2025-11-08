b5527d5eafeb6041ecd2d1f02e6e0bc5b07b6bb2
rajpatle2006 <rajpatle799@gmail.com>
2025-11-08 23:44:37 +0530

    java codes

---
+++ LEC 4/seachchar.java
@@ -0,0 +1,15 @@
+
+
+public class seachchar {
+    public static void main(String[] args) {
+        String str="hello";
+        int v=0;
+        for(char ch :str.toCharArray()){
+            if("aeuio".indexOf(ch)!=-1){
+                v++;
+            
+            }
+        }
+        System.out.println(v);
+    }
+}
