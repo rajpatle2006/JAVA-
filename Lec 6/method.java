63629295627038ac63f24b84b2c5fb58640f4de3
rajpatle2006 <rajpatle799@gmail.com>
2025-11-13 22:21:48 +0530

    Java codes

---
+++ Lec 6/method.java
@@ -0,0 +1,13 @@
+package Lec6;
+
+public class method {
+static void Sum(int a){
+    System.out.println("hii java");
+ }
+    public static void main(String[] args) {
+    method obj=new method();
+        obj.Sum(5);
+        
+    }
+    }
+
